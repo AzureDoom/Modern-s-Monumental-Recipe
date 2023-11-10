eb4d3ad8040243c4b6589051932106611e46655d
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2023-11-10 01:03:20 -0600

    v0.55 - Added Stone Rod - Bump Version

--- src/main/java/com/azuredoom/modernrecipes/ModernRecipesMod.java
+++ src/main/java/com/azuredoom/modernrecipes/ModernRecipesMod.java
@@ -49,6 +49,7 @@ public class ModernRecipesMod {
 		public static final RegistryObject<Item> COMPRESSED_IRON_PLATE = ITEMS.register("compressed_iron_plate", () -> new Item(new Item.Properties().tab(ModernRecipesMod.ITEMS_TAB)));
 		public static final RegistryObject<Item> TOOL_TOOLBOX = ITEMS.register("tool_toolbox", () -> new Item(new Item.Properties().tab(ModernRecipesMod.ITEMS_TAB)));
 		public static final RegistryObject<Item> PART_PLATE_SMALL_WOOD = ITEMS.register("part_plate_small_wood", () -> new Item(new Item.Properties().tab(ModernRecipesMod.ITEMS_TAB)));
+		public static final RegistryObject<Item> Stone_Rod = ITEMS.register("stone_rod", () -> new Item(new Item.Properties().tab(ModernRecipesMod.ITEMS_TAB)));
 
 		/**
 		 * This is just a simple block item, allowing you to register it for use in your recipes. Doesn't do anything fancy, is just a simple item that exists.
