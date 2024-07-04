e96f47bb925b6e0f009485b71d3205a08501f61e
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2024-07-04 15:15:21 -0500

    2nd comment of stuff being ported from 1.192

--- src/main/java/com/azure/modernrecipes/ModernRecipesMod.java
+++ src/main/java/com/azure/modernrecipes/ModernRecipesMod.java
@@ -51,22 +51,19 @@ public class ModernRecipesMod {
          */
         public static final Supplier<Item> COMPRESSED_IRON_PLATE = ITEMS.register("compressed_iron_plate", baseItem);
         public static final Supplier<Item> Tool_Toolbox = ITEMS.register("tool_toolbox", baseItem);
-        public static final Supplier<Item> Part_Mold = ITEMS.register("part_mold", baseItem);
-        public static final Supplier<Item> Part_Mold_Axe = ITEMS.register("part_mold_axe", baseItem);
-        public static final Supplier<Item> Part_Mold_Gear_1 = ITEMS.register("part_mold_gear_1", baseItem);
-        public static final Supplier<Item> Part_Mold_Gear_2 = ITEMS.register("part_mold_gear_2", baseItem);
-        public static final Supplier<Item> Part_Mold_Hammer = ITEMS.register("part_mold_hammer", baseItem);
-        public static final Supplier<Item> Part_Mold_Hoe = ITEMS.register("part_mold_hoe", baseItem);
-        public static final Supplier<Item> Part_Mold_Ingot = ITEMS.register("part_mold_ingot", baseItem);
-        public static final Supplier<Item> Part_Mold_Pickaxe = ITEMS.register("part_mold_pickaxe", baseItem);
-        public static final Supplier<Item> Part_Mold_Plate = ITEMS.register("part_mold_plate", baseItem);
-        public static final Supplier<Item> Part_Mold_Shovel = ITEMS.register("part_mold_shovel", baseItem);
-        public static final Supplier<Item> Part_Mold_Shovel_2 = ITEMS.register("part_mold_shovel_2", baseItem);
-        public static final Supplier<Item> Part_Mold_Sickle = ITEMS.register("part_mold_sickle", baseItem);
-        public static final Supplier<Item> Part_Mold_Sword = ITEMS.register("part_mold_sword", baseItem);
-        public static final Supplier<Item> Part_Plate_Small_Wood = ITEMS.register("part_plate_small_wood", baseItem);
+        public static final Supplier<Item> Stone_Rod  = ITEMS.register("stone_rod", baseItem);
+        public static final Supplier<Item> Acacia_Rod = ITEMS.register("acacia_rod", baseItem);
+        public static final Supplier<Item> Birch_Rod = ITEMS.register("birch_rod", baseItem);
+        public static final Supplier<Item> Dark_Oak_Rod = ITEMS.register("dark_oak_rod", baseItem);
+        public static final Supplier<Item> Jungle_Rod = ITEMS.register("jungle_rod", baseItem);
+        public static final Supplier<Item> Oak_Rod = ITEMS.register("oak_rod", baseItem);
+        public static final Supplier<Item> Spruce_Rod = ITEMS.register("spruce_rod", baseItem);
+        public static final Supplier<Item> Clay_Plate = ITEMS.register("clay_plate", baseItem);
         public static final Supplier<Item> Part_Plate_Large_Wood = ITEMS.register("part_plate_large_wood", baseItem);
+        public static final Supplier<Item> Part_Plate_Small_Wood = ITEMS.register("part_plate_small_wood", baseItem);
         public static final Supplier<Item> Part_Wood_Crosstie = ITEMS.register("part_wood_crosstie", baseItem);
+        public static final Supplier<Item> Part_Cog_Wood = ITEMS.register("part_cog_wood", baseItem);
+        public static final Supplier<Item> Part_Chainlinks = ITEMS.register("part_chainlinks", baseItem);
 
         /**
          * This is just a simple block item, allowing you to register it for use in your recipes. Doesn't do anything fancy, is just a simple item that exists.
@@ -94,40 +91,34 @@ public class ModernRecipesMod {
                 // Sets tab icon item or block
                 .icon(() -> new ItemStack(ModItems.COMPRESSED_IRON_PLATE.get())).icon(
                         () -> new ItemStack(ModItems.Tool_Toolbox.get())).icon(
-                        () -> new ItemStack(ModItems.Part_Mold.get())).icon(
-                        () -> new ItemStack(ModItems.Part_Mold_Axe.get())).icon(
-                        () -> new ItemStack(ModItems.Part_Mold_Gear_1.get())).icon(
-                        () -> new ItemStack(ModItems.Part_Mold_Gear_2.get())).icon(
-                        () -> new ItemStack(ModItems.Part_Mold_Hammer.get())).icon(
-                        () -> new ItemStack(ModItems.Part_Mold_Hoe.get())).icon(
-                        () -> new ItemStack(ModItems.Part_Mold_Ingot.get())).icon(
-                        () -> new ItemStack(ModItems.Part_Mold_Pickaxe.get())).icon(
-                        () -> new ItemStack(ModItems.Part_Mold_Plate.get())).icon(
-                        () -> new ItemStack(ModItems.Part_Mold_Shovel.get())).icon(
-                        () -> new ItemStack(ModItems.Part_Mold_Shovel_2.get())).icon(
-                        () -> new ItemStack(ModItems.Part_Mold_Sickle.get())).icon(
-                        () -> new ItemStack(ModItems.Part_Mold_Sword.get())).icon(
+                        () -> new ItemStack(ModItems.Stone_Rod.get())).icon(
+                        () -> new ItemStack(ModItems.Acacia_Rod.get())).icon(
+                        () -> new ItemStack(ModItems.Birch_Rod.get())).icon(
+                        () -> new ItemStack(ModItems.Dark_Oak_Rod.get())).icon(
+                        () -> new ItemStack(ModItems.Jungle_Rod.get())).icon(
+                        () -> new ItemStack(ModItems.Oak_Rod.get())).icon(
+                        () -> new ItemStack(ModItems.Spruce_Rod.get())).icon(
+                        () -> new ItemStack(ModItems.Clay_Plate.get())).icon(
                         () -> new ItemStack(ModItems.Part_Wood_Crosstie.get())).icon(
                         () -> new ItemStack(ModItems.Part_Plate_Large_Wood.get())).icon(
+                        () -> new ItemStack(ModItems.Part_Cog_Wood.get())).icon(
+                        () -> new ItemStack(ModItems.Part_Chainlinks.get())).icon(
                         () -> new ItemStack(ModItems.Part_Plate_Small_Wood.get()))
                 // Sets the items in the tab, in the order that you register them here
                 .displayItems((enabledFeatures, entries) -> {
                     entries.accept(ModItems.COMPRESSED_IRON_PLATE.get());
                     entries.accept(ModItems.Tool_Toolbox.get());
-                    entries.accept(ModItems.Part_Mold.get());
-                    entries.accept(ModItems.Part_Mold_Axe.get());
-                    entries.accept(ModItems.Part_Mold_Gear_1.get());
-                    entries.accept(ModItems.Part_Mold_Gear_2.get());
-                    entries.accept(ModItems.Part_Mold_Hammer.get());
-                    entries.accept(ModItems.Part_Mold_Hoe.get());
-                    entries.accept(ModItems.Part_Mold_Ingot.get());
-                    entries.accept(ModItems.Part_Mold_Pickaxe.get());
-                    entries.accept(ModItems.Part_Mold_Plate.get());
-                    entries.accept(ModItems.Part_Mold_Shovel.get());
-                    entries.accept(ModItems.Part_Mold_Shovel_2.get());
-                    entries.accept(ModItems.Part_Mold_Sickle.get());
-                    entries.accept(ModItems.Part_Mold_Sword.get());
+                    entries.accept(ModItems.Stone_Rod.get());
+                    entries.accept(ModItems.Acacia_Rod.get());
+                    entries.accept(ModItems.Birch_Rod.get());
+                    entries.accept(ModItems.Dark_Oak_Rod.get());
+                    entries.accept(ModItems.Jungle_Rod.get());
+                    entries.accept(ModItems.Oak_Rod.get());
+                    entries.accept(ModItems.Spruce_Rod.get());
+                    entries.accept(ModItems.Clay_Plate.get());
                     entries.accept(ModItems.Part_Wood_Crosstie.get());
+                    entries.accept(ModItems.Part_Cog_Wood.get());
+                    entries.accept(ModItems.Part_Chainlinks.get());
                     entries.accept(ModItems.Part_Plate_Small_Wood.get());
                     entries.accept(ModItems.Part_Plate_Large_Wood.get());
                     entries.accept(ModItems.EXAMPLE_BLOCK_ITEM.get());
